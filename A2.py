57a883366706a2dc9fc4c5259ee7812abb4b1077
JuliaFrazao <168451128+JuliaFrazao@users.noreply.github.com>
2024-05-28 14:47:02 -0300

    Update A2.py

--- A2.py
+++ A2.py
@@ -4,26 +4,26 @@ import pandas as pd
 # Carregar o dataset
 @st.cache
 def load_data():
-    df = pd.read_csv('NetFlix.csv')
+    df = pd.read_csv('movies.csv')
     return df
 
 # Carregar os dados
 df = load_data()
 
 # Título do aplicativo
-st.title("Busca de Filmes e Séries da Netflix por Ator/Atriz")
+st.title("Buscador de atores e atrizes de Hollywood")
 
 # Entrada do usuário para o nome do ator/atriz
 nome_ator = st.text_input("Digite o nome do ator ou atriz:")
 
 # Filtrar o dataset pelo nome inserido
 if nome_ator:
-    df_filtrado = df[df['cast'].str.contains(nome_ator, case=False, na=False, regex=False)]
+    df_filtrado = df[df['star'].str.contains(nome_ator, case=False, na=False, regex=False)]
     
     if not df_filtrado.empty:
         st.write(f"Filmes e Séries com {nome_ator}:")
-        st.dataframe(df_filtrado[['title', 'cast', 'type', 'country', 'release_year']])
+        st.dataframe(df_filtrado[['name', 'genre', 'year', 'country', 'star']])
     else:
-        st.write(f'Nenhum filme ou série encontrado com "{nome_ator}".')
+        st.write(f'Nenhum filme encontrado com "{nome_ator}".')
 else:
     st.write("Por favor, insira o nome de um ator ou atriz.")
