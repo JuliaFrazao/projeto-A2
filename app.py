904fda7209378f5d5ca77c689a76e1081137e256
JuliaFrazao <168451128+JuliaFrazao@users.noreply.github.com>
2024-06-04 15:11:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ contendo 7512 filmes. Divirta-se!
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://raw.githubusercontent.com/JuliaFrazao/projeto-A2/main/cortina vermelha.jpg");
+background-image: url("cortinavermelha.jpg");
 background-size: cover;
 }
 </style>
